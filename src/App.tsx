0229b2d815a7cf4f31ed2e60372116eb82c7b86b
Espen Sørby <espen@sorby.net>
2024-02-20 18:01:05 +0100

    Fetch and log out data from REST API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import './App.css'
+import { createClient } from "contentful"
+import { useEffect } from 'react';
+//import './App.css'
+
+const {
+  VITE_SPACE_ID,
+  VITE_ACCESS_TOKEN,
+  VITE_ENVIRONMENT,
+} = import.meta.env;
+
+const client = createClient({
+  accessToken: VITE_ACCESS_TOKEN,
+  space: VITE_SPACE_ID,
+  environment: VITE_ENVIRONMENT,
+})
 
 const App = () => {
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await client.getEntries({
+        content_type: "accordion",
+        include: 2,
+      })
+
+      const data = response.items;
+      console.log(data);
+      return data;
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <h1>FAQ</h1>
-    </>
+    <div>
+      FAQ
+    </div>
   )
 }
 
