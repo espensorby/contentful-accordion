6f7f02aa6c36ab8465f4c3b4db7c24dc1118b63c
Espen Sørby <espen@sorby.net>
2024-02-20 18:15:14 +0100

    Render out API data in a list

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,33 @@ const GET_ACCORDIONDATA = gql`
 `;
 
 const App = () => {
-  const { data } = useQuery(GET_ACCORDIONDATA);
+  const { loading = true, error, data } = useQuery(GET_ACCORDIONDATA);
   
-  console.log(data)
+  const renderedList = (data: any) => {
+    return data.accordionCollection.items.map((item: any) => {
+      return (
+        <div key={item.internalName}>
+          <h2>{item.title}</h2>
+          <ul>
+            {item.accordionItemsCollection.items.map((accordionItem: any) => (
+                <li key={accordionItem.sys.id}>
+                  <h3>{accordionItem.name}</h3>
+                  <p>{accordionItem.text}</p>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      )
+    })
+  }
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
   return (
     <div>
-      FAQ
+      {renderedList(data)}
     </div>
   )
 }
